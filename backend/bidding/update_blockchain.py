6bb5a1c5d2314f1fa31123dde048a93115b70ed0
manassri10 <shashwatsrivastava348@gmail.com>
2022-10-08 06:10:20 +0530

    update_blockchain.py me nai json daali hai

--- backend/bidding/update_blockchain.py
+++ backend/bidding/update_blockchain.py
@@ -12,82 +12,66 @@ land = {
         ]
     ]
 }  # dict  : map  : land - > wallet address
-chain = [
-    {
-        "data": [],
-        "difficulty": 3,
-        "hash": "genesis_hash",
-        "last_hash": "genesis_last_hash",
-        "nonce": "genesis_nonce",
-        "timestamp": 1
-    },
-    {
-        "data": [
-            {
-                "id": "dbdf1a3d",
-                "input": {
-                    "address": "6545496d",
-                    "amount": 1000,
-                    "public_key": "-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEse11YkhAtuxWof07DbuOzEdVAmwXhk9h\nS8doJonhYKa86dH+DdwfPvUwAtfc9pHJbgc6ExhljjQcCy12hyudyw==\n-----END PUBLIC KEY-----\n",
-                    "signature": [
-                        114864003191439788311665499359893570336855215874922046882684998988415914750468,
-                        77398801526828584707890912332243997625291502377432139494188339378892777684880
-                    ],
-                    "timestamp": 1665024613.768578
-                },
-                "output": {
-                    "amansdsaxena": "land2"
-                }
+block = {
+    "data": [
+        {
+            "id": "1b783113",
+            "input": {
+                "address": "5d853afc",
+                "amount": 1000,
+                "public_key": "-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEmLKr8GDEGiOOB9uNSHtTnovQuXEvxC7y\nQhZIjkgQssSBs0DeX8en24tjifkNpQZyISaUwnHjtK1xWm02r2Ullw==\n-----END PUBLIC KEY-----\n",
+                "signature": [
+                    75056617861011179515713774605111711153234082866727479373769021796619942151641,
+                    46661478515215909313479786139297717999930432091597056788606434116445030887352
+                ],
+                "timestamp": 1665188914.6071258
             },
-            {
-                "id": "94d04e12",
-                "input": {
-                    "address": "6545496d",
-                    "amount": 1000,
-                    "public_key": "-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEse11YkhAtuxWof07DbuOzEdVAmwXhk9h\nS8doJonhYKa86dH+DdwfPvUwAtfc9pHJbgc6ExhljjQcCy12hyudyw==\n-----END PUBLIC KEY-----\n",
-                    "signature": [
-                        53012783970614763388224069478103833104910280327665529485556180317800323971959,
-                        82705319049675579555615803259353111713125991665274134726606276077985902015500
-                    ],
-                    "timestamp": 1665024630.5904686
-                },
-                "output": {
-                    "amansdsaxena": "land1"
-                }
+            "output": {
+                "land": "land2",
+                "name_owner": "5d853afc"
             }
-        ],
-        "difficulty": 4,
-        "hash": "02ca2f510e73688712d323171592a89b94d3a7b3c109b108ae763e1551dcce43",
-        "last_hash": "genesis_hash",
-        "nonce": 0,
-        "timestamp": 1665024650.832157
-    },
-    {
-        "data": [
-            {
-                "id": "eaf3ff85",
-                "input": {
-                    "address": "6545496d",
-                    "amount": 1000,
-                    "public_key": "-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEse11YkhAtuxWof07DbuOzEdVAmwXhk9h\nS8doJonhYKa86dH+DdwfPvUwAtfc9pHJbgc6ExhljjQcCy12hyudyw==\n-----END PUBLIC KEY-----\n",
-                    "signature": [
-                        61811000501018983520234131876363830401011737395476025301834656897228061902398,
-                        20827783997069955309229581425658805813162713766080027846233703922646931106725
-                    ],
-                    "timestamp": 1665029348.7631187
-                },
-                "output": {
-                    "amansdsaxena": "land1"
-                }
+        },
+        {
+            "id": "2b086b34",
+            "input": {
+                "address": "5d853afc",
+                "amount": 1000,
+                "public_key": "-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEmLKr8GDEGiOOB9uNSHtTnovQuXEvxC7y\nQhZIjkgQssSBs0DeX8en24tjifkNpQZyISaUwnHjtK1xWm02r2Ullw==\n-----END PUBLIC KEY-----\n",
+                "signature": [
+                    104342705125858068560143175689311921098668404103301905151411255930959601263273,
+                    28464582692028894844217352818836186316537232803473648155365543761550678385226
+                ],
+                "timestamp": 1665189039.275466
+            },
+            "output": {
+                "land": "land1",
+                "name_owner": "5d853afc"
             }
-        ],
-        "difficulty": 5,
-        "hash": "01d66b8f4dac5b204b1323fea9cc25686b79142092c7333e5e646da93c800d7c",
-        "last_hash": "02ca2f510e73688712d323171592a89b94d3a7b3c109b108ae763e1551dcce43",
-        "nonce": 0,
-        "timestamp": 1665029380.872428
-    }
-]
+        },
+        {
+            "id": "9a7976f3",
+            "input": {
+                "address": "5d853afc",
+                "amount": 1000,
+                "public_key": "-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEmLKr8GDEGiOOB9uNSHtTnovQuXEvxC7y\nQhZIjkgQssSBs0DeX8en24tjifkNpQZyISaUwnHjtK1xWm02r2Ullw==\n-----END PUBLIC KEY-----\n",
+                "signature": [
+                    91456015900299781953986750717715325616136168850591984878911540672189079153733,
+                    72376059207270973314809171211548271551671818540859726669413474431030073732314
+                ],
+                "timestamp": 1665189044.6466753
+            },
+            "output": {
+                "land": "land3",
+                "name_owner": "5d853afc"
+            }
+        }
+    ],
+    "difficulty": 4,
+    "hash": "074cf6dd63bf9fb4484eaefd8c759fb827912975ed8d7e02e16ad90970d961ab",
+    "last_hash": "genesis_hash",
+    "nonce": 41,
+    "timestamp": 1665189057.95266
+}
 
-print(chain)
+print(block)
 
--- backend/tests/blockchain/old_test_block.py
+++ backend/tests/blockchain/old_test_block.py
@@ -1,5 +1,5 @@
 #from backend.blockchain.block import Block,GENESIS_DATA
-
+import json
 #def test_block():
 #    last_block = Block.genesis()
 #    data = 'tets-data'
@@ -14,4 +14,34 @@
 #    assert isinstance(genesis, Block)
  #   assert genesis.last_hash == GENESIS_DATA['last_hash']
   #  assert genesis.data == GENESIS_DATA['data']
-    
+
+mine_data = {
+    "data": [
+        {
+            "id": "53b67301",
+            "input": {
+                "address": "8d0fc811",
+                "amount": 1000,
+                "public_key": "-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEsjbgT2WyvxkpwbxT/Mr5otyvCitW//M+\nlg8DAM/A2sXPqEN0Qm9ujkj2Zcay6ASDNN6liTbIeWHfFE8gH7DtsQ==\n-----END PUBLIC KEY-----\n",
+                "signature": [
+                    54165874169992401299038840530400580266157154043395075056126550862577941035227,
+                    35348516953911841539557563541885200942735298352486087119883954860560164009526
+                ],
+                "timestamp": 1665141573.887527
+            },
+            "output": {
+                "land": "land1",
+                "name_owner": "8d0fc811"
+            }
+        }
+    ],
+    "difficulty": 5,
+    "hash": "05f8a77823f44937189f800924e769b9e2d7611a38501c7b11453509a012d3e0",
+    "last_hash": "0a70f86e5c08712ab0a8e9b4c4d3c22041514173ed17228ef5cc40300855df30",
+    "nonce": 17,
+    "timestamp": 1665141591.058639
+}
+#final_data = dict()
+print(mine_data)
+
+
